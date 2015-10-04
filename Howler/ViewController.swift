a0980ae40074c40b77284207840d9a172a164555
Tracy Lam <tracylam@airbears2-10-142-33-150.airbears2.1918.berkeley.edu>
2015-10-03 17:17:47 -0700

    Initial Commit

---
+++ Howler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Howler
+//
+//  Created by Tracy Lam on 10/3/15.
+//  Copyright © 2015 Sharfa Prasad and Tracy Lam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
