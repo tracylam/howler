c05d33329bb702405a61b082fc5978764aa18b1e
Tracy Lam <tracylam@berkeley.edu>
2018-08-15 14:03:32 -0700

    add proj

--- Howler/ViewController.swift
+++ Howler/ViewController.swift
@@ -7,18 +7,52 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var audioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //self.view.backgroundColor = UIColor(patternImage: UIImage(named: "URGE.pdf")!)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func makeNoise(sender: AnyObject) {
+        
+        //set file name and location
+        let loudNoise = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("2", ofType: "mp3")!)
+        
+        do{
+        //tell app to prepare audio file
+        
+        try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+        } catch _ {
+        
+        }
+        
+        do {
+            
+        try AVAudioSession.sharedInstance().setActive(true)
+        } catch _ {
+            
+        }
+
+        
+        audioPlayer = try! AVAudioPlayer(contentsOfURL: loudNoise)
+        audioPlayer.prepareToPlay()
+        audioPlayer.play()
+        
+        
+        
+    }
+    
 
 
 }
